b94ecdab8468a3b2b5a66dd19213f0c06f68de89
Denis Rystsov <derystso@microsoft.com>
2018-11-16 18:20:00 -0800

    working on online checker

--- cloud/consistency-eternal/start-checker.js
+++ cloud/consistency-eternal/start-checker.js
@@ -1,3 +1,78 @@
+class FictionHistoryError extends Error {
+    constructor(...args) {
+        super(...args)
+        Error.captureStackTrace(this, FictionHistoryError)
+    }
+}
+
+class RegisterChecker {
+    constructor() {
+
+    }
+
+    beginWrite(time, processId, prev, next, value) {
+        // put to waiting set
+    }
+    endWrite(time, processId) {
+        // go backward and move chain from waiting to accepted
+
+        // 
+        
+        // going backwards from the begining of this write
+        // an remove from waiting set, they 
+    }
+    conflictWrite(time, processId) {}
+
+    beginRead(time, processId) { }
+    endRead(time, processId, writeID, value) {}
+}
+
+class OnlineChecker {
+    constructor() {
+        this.keyCheckers = new Map();
+        this.lastKey = new Map();
+    }
+
+    beginWrite(key, time, processId, prev, next, value) {
+        this.lastKey.set(processId, key);
+        this.getChecker(key).beginWrite(time, processId, prev, next, value);
+    }
+    endWrite(time, processId) {
+        const key = this.getLastKey(processId)
+        this.getChecker(key).endWrite(time, processId);
+    }
+    conflictWrite(time, processId) {
+        const key = this.getLastKey(processId)
+        this.getChecker(key).conflictWrite(time, processId);
+    }
+
+    beginRead(key, time, processId) {
+        this.lastKey.set(processId, key);
+        this.getChecker(key).beginRead(time, processId);
+    }
+    endRead(time, processId, writeID, value) {
+        const key = this.getLastKey(processId)
+        this.getChecker(key).endRead(time, processId, writeID, value);
+    }
+
+    getLastKey(processId) {
+        if (!this.lastKey.has(processId)) {
+            throw new FictionHistoryError("Can't end a write which wasn't started");
+        }
+        const key = this.lastKey.get(processId);
+        this.lastKey.delete(processId);
+        return key;
+    }
+
+    getChecker(key) {
+        if (!this.keyCheckers.has(key)) {
+            this.keyCheckers.set(key, new RegisterChecker());
+        }
+        return this.keyCheckers.get(key);
+    }
+}
+
+
 const lineReader = require("readline").createInterface({
     input: require("fs").createReadStream("history.log")
 });
@@ -29,7 +104,7 @@ lineReader.on("line", function (line) {
         const time = parseInt(parts[0]);
         const processId = parseInt(parts[1]);
     } else {
-        throw new Error("FUCK: " + line);
+        throw new Error("Something unknown" + line);
     }
     
     lines += 1;
