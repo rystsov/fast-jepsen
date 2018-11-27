9cc28b153fde8ff8d35c5017a1fb03b60ad73ac4
Denis Rystsov <rystsov.denis@gmail.com>
2018-11-27 14:30:00 -0800

    wip

--- cloud/consistency-eternal/start-checker.js
+++ cloud/consistency-eternal/start-checker.js
@@ -79,7 +79,6 @@ class RegisterChecker {
     }
 
     endWrite(time, processId) {
-        //console.info(`LOG: endWrite(${time}, ${processId})`);
         if (this.time >= time) {
             throw new WrongHistoryError(`Time must go forward got ${time} but ${this.time} is already known`);
         }
@@ -451,8 +450,86 @@ function writeReadWrongWriteIDTest() {
     });
 }
 
+function writeFailReadNewTest() {
+    const checker = new OnlineChecker("0000", 0);
+    const clock = new Clock(1);
 
+    checker.beginWrite("key1", clock.tick(), 0, "0000", "0001", 1);
+    checker.failWrite(clock.tick(), 0);
 
+    checker.beginRead("key1", clock.tick(), 1);
+    checker.endRead(clock.tick(), 1, "0001", 1);
+}
+
+function writeFailReadOldTest() {
+    const checker = new OnlineChecker("0000", 0);
+    const clock = new Clock(1);
+
+    checker.beginWrite("key1", clock.tick(), 0, "0000", "0001", 1);
+    checker.failWrite(clock.tick(), 0);
+
+    checker.beginRead("key1", clock.tick(), 1);
+    checker.endRead(clock.tick(), 1, "0000", 0);
+}
+
+function writeFailReadOldNewTest() {
+    const checker = new OnlineChecker("0000", 0);
+    const clock = new Clock(1);
+
+    checker.beginWrite("key1", clock.tick(), 0, "0000", "0001", 1);
+    checker.failWrite(clock.tick(), 0);
+
+    checker.beginRead("key1", clock.tick(), 1);
+    checker.endRead(clock.tick(), 1, "0000", 0);
+
+    checker.beginRead("key1", clock.tick(), 1);
+    checker.endRead(clock.tick(), 1, "0001", 1);
+}
+
+function writeFailReadOldNewOldTest() {
+    const checker = new OnlineChecker("0000", 0);
+    const clock = new Clock(1);
+
+    checker.beginWrite("key1", clock.tick(), 0, "0000", "0001", 1);
+    checker.failWrite(clock.tick(), 0);
+
+    checker.beginRead("key1", clock.tick(), 1);
+    checker.endRead(clock.tick(), 1, "0000", 0);
+
+    checker.beginRead("key1", clock.tick(), 1);
+    checker.endRead(clock.tick(), 1, "0001", 1);
+
+    checker.beginRead("key1", clock.tick(), 1);
+    
+    expectViolation(() => checker.endRead(clock.tick(), 1, "0000", 0));
+}
+
+function forkTest() {
+    // 0000 <- 0001 <- 0002
+    // \                    <- 0003 <- 0004 (*)
+    
+    const checker = new OnlineChecker("0000", 0);
+    const clock = new Clock(1);
+
+    checker.beginWrite("key1", clock.tick(), 0, "0000", "0001", 11);
+    checker.beginWrite("key1", clock.tick(), 1, "0001", "0002", 12);
+    checker.beginWrite("key1", clock.tick(), 2, "0000", "0003", 21);
+    checker.beginWrite("key1", clock.tick(), 3, "0003", "0004", 22);
+    
+    checker.beginRead("key1", clock.tick(), 4);
+    checker.endRead(clock.tick(), 4, "0004", 22);
+
+    expectViolation(() => checker.endWrite(clock.tick(), 0));
+    expectViolation(() => checker.endWrite(clock.tick(), 1));
+    checker.endWrite(clock.tick(), 3);
+    checker.endWrite(clock.tick(), 2);
+}
+
+// forkTest();
+// writeFailReadNewTest();
+// writeFailReadOldTest();
+// writeFailReadOldNewTest();
+// writeFailReadOldNewOldTest();
 // readWriteParOldTest();
 // readWriteParNewTest();
 // writeReadParOldTest();
