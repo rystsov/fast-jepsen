1d23040ed3a7c06f25b62d4e3dbbd628919b8df6
Denis Rystsov <derystso@microsoft.com>
2018-12-03 17:47:41 -0800

    decoupled settings and code

--- cloud/webkv-mongodb/start.js
+++ cloud/webkv-mongodb/start.js
@@ -1,16 +1,21 @@
 const {MongoKV} = require("./src/MongoKV");
 const {RemoteTesterServer} = require("./src/RemoteTesterServer");
+const fs = require("fs");
+const process = require("process");
 
-async function start() {
+async function start(settings) {
     const kvs = new MongoKV(
-        "rystsov-mongo-42.documents.azure.com",
-        10255,
-        "rystsov-mongo-42",
-        ""
+        settings.mongodb.host,
+        settings.mongodb.port,
+        settings.mongodb.userName,
+        settings.mongodb.pwd,
+        settings.mongodb.poolSize
     );
     await kvs.init();
-    const service = new RemoteTesterServer(kvs, 13452);
+    const service = new RemoteTesterServer(kvs, settings.port);
     service.start();
 }
 
-start()
+let settings = fs.readFileSync(process.argv[2], "utf8");
+settings = JSON.parse(settings);
+start(settings);
